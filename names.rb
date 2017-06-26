95cb92ba42c27360113dfe72a5f3a869bc47841e
debgolick <shock1142003@atlanticbb.net>
2017-06-26 13:05:54 -0400

    names

---
+++ names.rb
@@ -0,0 +1,11 @@
+def fname(name)	 p "your first name is #{name}"
+end
+def lname(lname)
+  p "and your last name is #{lname}"
+  end
+   fname("Debbie")
+   lname("Golick")
+   #fname("Jane")
+   #lname("Doe")
+   #fname("Jon")
+   #lname("Doe")
